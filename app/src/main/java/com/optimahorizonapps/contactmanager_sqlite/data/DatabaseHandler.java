5ffb0f302645517cd0443fe32b9395c91dfd6e34
leonardo1313 <leonardo.novak5@gmail.com>
2021-09-09 11:04:47 +0200

    DatabaseHandler class added - onCreate and onUpgrade methods declared

--- app/src/main/java/com/optimahorizonapps/contactmanager_sqlite/data/DatabaseHandler.java
+++ app/src/main/java/com/optimahorizonapps/contactmanager_sqlite/data/DatabaseHandler.java
@@ -4,21 +4,35 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+
+import com.optimahorizonapps.contactmanager_sqlite.R;
+import com.optimahorizonapps.contactmanager_sqlite.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
+    //This is where we create our table
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "(" +
+                Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + " TEXT," +
+                Util.KEY_PHONE_NUMBER + " TEXT" + ")";
 
+        db.execSQL(CREATE_CONTACT_TABLE); //this is where table is created
     }
 
+    //Deleting table and replacing it with another one
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Deleting the table
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+
+        //Creating new one
+        onCreate(db);
 
     }
 }
